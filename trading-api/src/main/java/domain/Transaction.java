7c6f559ed1f93a7f6d448ff7a6a336f1cbb02652
jean-airoldie <maxence.caron.1@ulaval.ca>
2018-09-27 18:21:55 -0400

    Fixed Transaction that would not compile

--- trading-api/src/main/java/domain/Transaction.java
+++ trading-api/src/main/java/domain/Transaction.java
@@ -6,15 +6,10 @@ public class Transaction {
     private Stock stock;
     private Long quantity;
 
-
     public void Transaction(Long accountNumber, TransactionType transactionType, Stock stock, Long quantity){
         this.accountNumber = accountNumber;
         this.transactionType = transactionType;
         this.stock = stock;
         this.quantity = quantity;
     }
-
-    public
-
-
 }
