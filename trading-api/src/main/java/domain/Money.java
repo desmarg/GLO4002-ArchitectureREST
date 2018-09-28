d5b270e81515cd51ea33fcff477db314670bc1b7
Antoine <antoine.somma.1@gmail.com>
2018-09-28 17:50:35 -0400

    Pre merge commit
    Cleaning branch to pull feature 1 commits.

--- trading-api/src/main/java/domain/Money.java
+++ trading-api/src/main/java/domain/Money.java
@@ -2,12 +2,28 @@ package domain;
 
 import java.math.BigDecimal;
 
-public class Money {
+public class Money implements Comparable<Money> {
     private BigDecimal amount;
 
     public Money(){
         this.amount = new BigDecimal(0);
     }
 
+    public Money(BigDecimal amount){
+        this.amount = new BigDecimal(0);
+    }
 
+    public void multiply(Long quantity) {
+        BigDecimal rhs = new BigDecimal(quantity);
+        this.amount = this.amount.multiply(rhs);
+    }
+
+    public void add(Money money){
+        this.amount = this.amount.add(money.amount);
+    }
+
+    @Override
+    public int compareTo(Money money) {
+        return 0;
+    }
 }
--- trading-api/src/main/java/domain/Stock.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class Stock {
-    private String market;
-    private String symbol;
-    private StockType stockType;
-    private Money price;
-    private DateTime date;
-}
