38596482c66000cbda623010fc0c11214ba301e9
Guillaume D De Grandpre <guillaume@geohotspot.com>
2018-10-08 15:28:26 -0400

    Add tests for TransactionSell

--- trading-api/src/main/java/trading/api/request/TransactionPostRequest.java
+++ trading-api/src/main/java/trading/api/request/TransactionPostRequest.java
@@ -3,5 +3,4 @@ package trading.api.request;
 import trading.api.response.TransactionResponse;
 
 public class TransactionPostRequest extends TransactionResponse {
-
 }
--- trading-api/src/test/java/trading/domain/transaction/TransactionBuyTest.java
+++ trading-api/src/test/java/trading/domain/transaction/TransactionBuyTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.Matchers.*;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import trading.domain.Account;
@@ -16,8 +15,7 @@ import trading.exception.NotEnoughCreditsException;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -39,46 +37,61 @@ public class TransactionBuyTest {
     private Credits SOME_STOCK_PRICE = new Credits(new BigDecimal(123));
 
     @Test(expected = InvalidQuantityException.class)
-    public
-    void givenBuyQuantitySmallerThanOne_whenMakingTransaction_thenThrowInvalidQuantityException() {
-        when(account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
-        TransactionBuy transactionBuy = new TransactionBuy(INVALID_QUANTITY, VALID_DATE, stock,
-                SOME_STOCK_PRICE);
+    public void givenBuyQuantitySmallerThanOne_whenMakingTransaction_thenThrowInvalidQuantityException() {
+        when(this.account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
+        TransactionBuy transactionBuy = new TransactionBuy(this.INVALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
 
-        transactionBuy.make(account);
+        transactionBuy.make(this.account);
     }
 
     @Test(expected = NotEnoughCreditsException.class)
     public void givenNotEnoughCredits_whenMakingTransaction_thenThrowNotEnoughCreditsException() {
-        when(account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(false);
-        TransactionBuy transactionBuy = new TransactionBuy(VALID_QUANTITY, VALID_DATE, stock,
-                SOME_STOCK_PRICE);
+        when(this.account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(false);
+        TransactionBuy transactionBuy = new TransactionBuy(this.VALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
 
-        transactionBuy.make(account);
+        transactionBuy.make(this.account);
     }
 
     @Test
     public void
-            givenValidTransaction_whenMakingTransaction_thenSubtractTotalPriceFromAccount() {
-        when(account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
-        TransactionBuy transactionBuy = new TransactionBuy(VALID_QUANTITY, VALID_DATE, stock,
-                SOME_STOCK_PRICE);
+    givenValidTransaction_whenMakingTransaction_thenSubtractTotalPriceFromAccount() {
+        when(this.account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
+        TransactionBuy transactionBuy = new TransactionBuy(this.VALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
 
-        transactionBuy.make(account);
+        transactionBuy.make(this.account);
 
         Credits expectedTotalPrice = transactionBuy.getTotalPrice();
-        verify(account).subtractCredits(creditsArgumentCaptor.capture());
-        assertEquals(expectedTotalPrice.getAmount(), creditsArgumentCaptor.getValue().getAmount());
+        verify(this.account).subtractCredits(this.creditsArgumentCaptor.capture());
+        assertEquals(expectedTotalPrice.getAmount(), this.creditsArgumentCaptor.getValue().getAmount());
     }
 
     @Test
     public void givenValidTransaction_whenMakingTransaction_thenTransactionIsAddedToAccount() {
-        when(account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
-        TransactionBuy transactionBuy = new TransactionBuy(VALID_QUANTITY, VALID_DATE, stock,
-                SOME_STOCK_PRICE);
+        when(this.account.hasEnoughCreditsToPay(any(Credits.class))).thenReturn(true);
+        TransactionBuy transactionBuy = new TransactionBuy(this.VALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
 
-        transactionBuy.make(account);
+        transactionBuy.make(this.account);
 
-        verify(account).addTransaction(transactionBuy);
+        verify(this.account).addTransaction(transactionBuy);
+    }
+
+    @Test
+    public void givenNotEnoughStocks_whenCheckingEnoughStocks_thenReturnFalse() {
+        TransactionBuy transactionBuy = new TransactionBuy(this.VALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
+
+        assertFalse(transactionBuy.hasEnoughStock(this.VALID_QUANTITY + 1));
+    }
+
+    @Test
+    public void givenEnoughStocks_whenCheckingEnoughStocks_thenReturnTrue() {
+        TransactionBuy transactionBuy = new TransactionBuy(this.VALID_QUANTITY, this.VALID_DATE, this.stock,
+                this.SOME_STOCK_PRICE);
+
+        assertTrue(transactionBuy.hasEnoughStock(this.VALID_QUANTITY - 1));
     }
 }
