a5e684c89d249936ace334e5f4512ba3c0122bdd
jean-airoldie <maxence.caron.1@ulaval.ca>
2018-11-17 08:36:59 -0500

    Removed infra-tests

--- infra-test/Exceptions.py
+++
@@ -1,5 +0,0 @@
-class HTTP_ERROR(Exception):
-    def __init__(self, message):
-
-        # Call the base class constructor with the parameters it needs
-        super(HTTP_ERROR, self).__init__(message)
